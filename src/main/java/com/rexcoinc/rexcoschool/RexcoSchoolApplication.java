538d35ded129dc1382633cb3e35ab53c609f5a3c
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2022-12-15 05:21:55 +0100

    Added project set up

---
+++ src/main/java/com/rexcoinc/rexcoschool/RexcoSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.rexcoinc.rexcoschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RexcoSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RexcoSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rexcoinc/rexcoschool/RexcoSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rexcoinc.rexcoschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RexcoSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
